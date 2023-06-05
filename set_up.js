85df6f4fd31d6c7e51670b0acaaf32e290c3178b
Shawn <79812606+Caloverys@users.noreply.github.com>
2023-06-05 14:35:05 -0400

    Update set_up.js

--- set_up.js
+++ set_up.js
@@ -14,28 +14,36 @@ for (let j = 0; j < row_num; j++) {
 }
 
 function apply_animation() {
+  console.clear()
+
   //debugger
+    console.log(table.querySelectorAll('th'))
 table.querySelectorAll('th').forEach(th=>{
  // th.querySelector("div").style.animationDelay = "0s"
   //th.querySelector("div").style.animationDelay = '0s'
-  //th.querySelector("div").classList.remove("opacity_animation", "opacity_animation_reverse","low_opacity");
-  //th.classList.remove("border_opacity_animation", "border_opacity_animation_reverse","low_border_color")
+  th.querySelector("div").classList.remove("opacity_animation", "opacity_animation_reverse","low_opacity");
+  th.classList.remove("border_opacity_animation", "border_opacity_animation_reverse","low_border_color","high_border_color")
+  th.classList.remove("high_border_color")
 })
+    console.log(table.querySelectorAll('th'))
   table.querySelectorAll("th").forEach((th, num) => {
-    //console.log(num, row_num * col_num-1)
+   // //console.log(num, row_num * col_num-1)
+   // console.log(th,num)
+
     th.querySelector("div").classList.add("opacity_animation");
     th.classList.add("border_opacity_animation");
-    console.log(Math.floor(num/col_num)/20)
+    console.log(num,th)
     th.style.animationDelay = `${Math.floor(num/col_num)/20 + 0.05}s`;
    // th.classList.add("high_border_color");
+    console.log(num,th)
+   
     th.addEventListener("animationend", function() {
+    th.classList.remove("low_border_color")
     th.classList.add("high_border_color");
 
-      console.log("oh yees", num)
-
+      //console.log("oh yees", num, th, table.querySelectorAll("th"))
       if (num === row_num * col_num - 1) {
         //debugger
-        console.log("shit")
         Array.from(table.querySelectorAll("th")).reverse().forEach((th, num) => {
           th.classList.add("border_opacity_animation_reverse");
           th.style.animationDelay = `${Math.floor(num/col_num)/20 +0.05}s`;
